16672b4c3fede126b0ee3bb5c9d9b99d8bd530dd
Henry <adjeihenry01@gmail.com>
2024-04-02 15:17:07 +0200

    Main error fix

--- intro_mobile_project/lib/main.dart
+++ intro_mobile_project/lib/main.dart
@@ -1,8 +1,7 @@
-
+import 'package:flutter/material.dart';
 import 'package:intro_mobile_project/screens/signin_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
