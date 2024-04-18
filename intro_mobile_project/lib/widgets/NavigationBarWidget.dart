e8d113616624194b1ec25ee30c99aff6200df587
Henry <adjeihenry01@gmail.com>
2024-04-18 12:05:15 +0200

    Navigating between pages added

--- intro_mobile_project/lib/widgets/NavigationBarWidget.dart
+++ intro_mobile_project/lib/widgets/NavigationBarWidget.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
-class NavigationBar extends StatelessWidget {
+class NavigationBar extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.explore),
-          label: 'Explore',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
-      selectedItemColor: Colors.white,
-      backgroundColor: Color.fromARGB(255, 245, 90, 79),
-      unselectedItemColor: Colors.black,
-    );
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 245, 90, 79),
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped);
   }
 }
