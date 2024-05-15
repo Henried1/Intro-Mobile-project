b1400dee4510870dc00cc1d469b39bdec2e7789d
Henry <adjeihenry01@gmail.com>
2024-05-15 16:17:58 +0200

    more information in reservationlist

--- intro_mobile_project/lib/screens/reservationsList_screen.dart
+++ intro_mobile_project/lib/screens/reservationsList_screen.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ReservationListScreen extends StatefulWidget {
-  const ReservationListScreen({super.key});
+  const ReservationListScreen({Key? key}) : super(key: key);
 
   @override
   State<ReservationListScreen> createState() => _ReservationListScreen();
@@ -15,8 +15,8 @@ class _ReservationListScreen extends State<ReservationListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Reservations'),
-      ),
+          title: Text('Your Reservations'),
+          backgroundColor: const Color.fromARGB(255, 245, 90, 79)),
       body: StreamBuilder<QuerySnapshot>(
           stream: _Reservations,
           builder:
@@ -30,11 +30,33 @@ class _ReservationListScreen extends State<ReservationListScreen> {
             return ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot doc) {
                 Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-                return ListTile(
-                    title: Text(data['Field']),
-                    subtitle: Text(
-                      data['Date: '].toDate().toString(),
-                    ));
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['Field'],
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          data['Location'],
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        Text(
+                          data['Date: '].toDate().toString(),
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        Text(
+                          'Players: ${data['Players']}',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
               }).toList(),
             );
           }),
